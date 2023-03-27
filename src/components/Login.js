948732add1feedf07efd8e5e74e214da04891d8f
wrightdj99 <wrightdj99@gmail.com>
2023-03-27 17:26:24 -0500

    Fixed Text Fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { login, logout } from '../features/user';
+import { useState } from 'react';
+
 function Login() {
+    const [userName, setName] = useState("");
+    const [userAge, setAge] = useState(0);
+    const [userEmail, setEmail] = useState("");
     //This is a Hook that's going to look through our imported slice to find a matching reducer, and then call it.
     const dispatch = useDispatch();
-
     return (
         <div>
+            <p>Name</p>
+            <input type="text" name="name" onChange={(event) => setName(event.target.value)}/><br/>
+            <p>Age</p>
+            <input type='number' name="age" onChange={(event) => setAge(event.target.value)}/><br/>
+            <p>Email</p>
+            <input type="text" name="email" onChange={(event) => setEmail(event.target.value)}/><br/>
             <button onClick={()=>{
-                dispatch(login({name: "Dan", age: 24, email: "wright.hinsdale99@hotmail.com"}));
+                dispatch(login({name: userName, age: userAge, email: userEmail}));
             }}
             >Login</button>
             <br/>
