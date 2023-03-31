d910822fac0d8eb4f09c4ef4ae5aab7117d5f9a7
wrightdj99 <wrightdj99@gmail.com>
2023-03-31 17:40:05 -0500

    Last minute comments

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,10 @@ function Login() {
     const [userEmail, setEmail] = useState("");
     //This is a Hook that's going to look through our imported slice to find a matching reducer, and then call it.
     const dispatch = useDispatch();
+    //If it seems a bit weird that we're using custom hooks alongside calls to our reducers, there is an explanation!
+    //Hooks like setName, setAge or setEmail will just monitor the info entered into the text fields, and this eventually
+    //becomes our payload that's fed to the user slice's reducers (login/out). Slices are good for global functionality, but as
+    //far as I'm concerned, hooks that call useState() are still king when it comes to just keeping track of text fields
     return (
         <div>
             <p>Name</p>
